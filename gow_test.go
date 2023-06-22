272964499d5009b112bfc48fd54eb79183b55412
lifezq <lifezqy@126.com>
2023-06-22 16:37:02 +0800

    update

--- gow_test.go
+++ gow_test.go
@@ -2,21 +2,6 @@ package gow
 
 import "testing"
 
-type ControllerTest Controller
-
-func (c *ControllerTest) HelloAction() {
-	c.Response.RenderString("Hello World")
-}
-
-func TestGow(t *testing.T) {
-	s := New()
-	t.Logf("s:%v\n", s)
-	s.SetBaseUrl("/test")
-	s.RegisterController("/ct", &ControllerTest{})
-
-	s.Run(":16000")
-}
-
 func TestNew(t *testing.T) {
 
 	if s := New(); s == nil || s.config == nil {
